53dca632613543f6585a5e20c39a4b6328f71098
Julie <148833273+jjade03@users.noreply.github.com>
2025-04-24 20:48:11 -0400

    Add files via upload

--- PlantShelf.cs
+++ PlantShelf.cs
@@ -1,48 +1,64 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace PlantInformationProject {
     class PlantShelf {
         static void Main(string[] args) {
-            Console.Write("Welcome to your plant shelf! What would you like to do?\n1. View Plants\n2. Add New Plant\n3. View Infirmary\n4. View Graveyard" +
-            "Enter the number corresponding with your selection: ");
-            int option = Convert.ToInt32(Console.ReadLine());
+            // Creates a path and checks if the file already exists. If it doesn't, creates the file and closes it.
+            string path = @"PlantList.txt";
+            if(!File.Exists(path)) {
+                File.Create(path).Close();
+            }
+            /*Console.Write("Welcome to your plant shelf! What would you like to do?\n1. View Plants\n2. Add New Plant\n3. View Infirmary\n4. View Graveyard" +
+            "\n\nEnter the number corresponding with your selection: ");
+            int option = Convert.ToInt32(Console.ReadLine());*/
             
-            plantInfo(); // Prompts the user to enter their plant's information
+            plantInfo(path); // Prompts the user to enter their plant's information
         }
 
         // Future goal: Rather than have user enter any data they want, have them select from provided options unless stated otherwise
-        static void plantInfo() {
+        static void plantInfo(string path) {
+            // Creates a FileStream object to write to the text file.
+            string plantInfo = "";
+
+            /* RECEIVES USER INPUT [START] */
             Console.Write("Enter Plant Species: ");
-            string plantSpecies = Console.ReadLine();
+            string plantSpecies = Console.ReadLine() ?? "null";
 
             Console.Write("Enter Plant's Age: ");
             int plantAge = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter Watering Frequency: ");
-            string waterFrequency = Console.ReadLine();
+            string waterFrequency = Console.ReadLine() ?? "null";
 
             Console.Write("Enter Sunlight Requirement: ");
-            string sunRequirement = Console.ReadLine();
+            string sunRequirement = Console.ReadLine() ?? "null";
 
             Console.Write("Enter Your Region: ");
-            string userRegion = Console.ReadLine();
+            string userRegion = Console.ReadLine() ?? "null";
 
             Console.Write("Enter Plant's Location: ");
-            string plantLocation = Console.ReadLine();
+            string plantLocation = Console.ReadLine() ?? "null";
 
             Console.Write("Does your plant have a nickname? (Enter 'yes' or 'no'): ");
-            string checkName = Console.ReadLine();
+            string checkName = Console.ReadLine() ?? "null";
 
             if(checkName == "yes") {
                 Console.Write("Enter Nickname: ");
-                string plantNickname = Console.ReadLine();
+                string plantNickname = Console.ReadLine() ?? "null";
 
-                // Heading for output. TO DO: If no nickname, make species the heading
-                Console.WriteLine("\n" plantNickname + "'s Information:");
+                // TO DO: If no nickname, make species the heading
+                Console.WriteLine("\n" + plantNickname + "'s Information:");
+                // Inserts the plants nickname at the end of the file
+                plantInfo = plantNickname + ",";
             }
-            // TO DO: Tidy up output so user inputs are spaced out evenly
-            Console.WriteLine("Plant Species: " + plantSpecies + "\nAge: " + plantAge + "\nWatering Frequency: " + waterFrequency
-            + "\nSunlight Requirements: " + sunRequirement + "\nRegion: " + userRegion + "\nLocation: " + plantLocation);
+            /* RECEIVE USER INPUT [END] */
+            // Appends user's plant information to the string.
+            plantInfo += plantSpecies + "," + plantAge + "," + waterFrequency + "," + sunRequirement + "," + userRegion + "," + plantLocation + "\n";
+
+            // Write information to the file, then close it.
+            File.AppendAllText(path, plantInfo);
         }
     }
 }
