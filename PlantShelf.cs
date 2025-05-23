cc5cb4fdc1c03a742dda4d39ccd8682448ed13ce
Julie <148833273+jjade03@users.noreply.github.com>
2025-05-22 21:12:08 -0400

    Fixed bugs.
    Changed the console output quantity per line to be restricted based on the size of the console.

--- PlantShelf.cs
+++ PlantShelf.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks.Dataflow;
 namespace PlantInformationProject {
     class PlantShelf {
         static void Main(string[] args) {
-            // Creates a path and checks if the file already exists. If it doesn't, creates the file and closes it.
+            int option = -1;        // Initializes 'option' as an invalid number.
+            string optionPrompt = " Enter a valid option (1-2): ";
+
             string path = @"PlantList.txt";
+
+            // Creates a path and checks if the file already exists. If it doesn't, creates the file and closes it.
             if(!File.Exists(path)) {
                 File.Create(path).Close();
             }
 
             Console.Write("Welcome to your plant shelf! What would you like to do?\n1. View Plants\n2. Add New Plant" +
             "\n\nEnter the number corresponding with your selection: ");
-            int option = Convert.ToInt32(Console.ReadLine());
+
+            // Ensures user enters a valid option.
+            while(option <= 0 || option > 2) {
+                try {
+                    option = Convert.ToInt32(Console.ReadLine());
+                    if(option <= 0 || option > 2) {
+                        Console.Write("Invalid option entered." + optionPrompt);
+                    }
+                } catch (Exception e) {
+                    Console.Write(e.Message + optionPrompt);
+                }
+            }
             
             switch(option) {
                 case 1: 
@@ -137,17 +152,26 @@ namespace PlantInformationProject {
         /* Formats and outputs the user's plant information*/
         // TO DO: Simplify and improve efficiency of method
         private static void outputPlantInfo(string path) {
+            /* Formatting variables */
             // Divides the file based on the parameters provided in 'separators'.
             string fileContent = File.ReadAllText(path);
             string[] dividedInfo = fileContent.Split(",", StringSplitOptions.RemoveEmptyEntries);
 
             string[] formatInfo = new string[dividedInfo.Length];          // Holds the contents of the file with uppercases.
-
+            
             int numElements = 6;                                           // Max number of variables entered into file on each line.
             int fileRows = File.ReadLines(path).Count() - 1;               // Gets the numbers of lines in the file.
             int countElem = 0;                                             // Holds the current actual element position.
             int countCols = 0;                                      
             string[,] readPlantInfo = new string[numElements, fileRows];   // Formatted version of the file contents.
+
+            /* Output Variables */
+            int perRow = Console.WindowWidth/80;                           // Defines the max number of columns relative to the console's width.
+            int evenRows = readPlantInfo.GetLength(1)/2;                   // Defines the amount of full rows.
+            int y = 0;                                                     // Initializes the starting index.
+            int startIndex = 0;                                            // Saves the starting position for 'y'.
+            int maxIndex = perRow;                                         // Defines the max index to be printed on a single row in the console.
+            int countRows = 0;                                             // Holds the number of rows printed to the console.
 
             // Increments through each element in the array, making the first character of each element uppercase.
             FormatOutput(dividedInfo, formatInfo);
@@ -161,25 +185,19 @@ namespace PlantInformationProject {
                 countElem++;
                 countCols = 0;
             }
-
-            int evenRows = readPlantInfo.GetLength(1)/2;    // Defines the amount of full rows.
-            int y = 0;                                      // Initializes the starting index.
-            int startIndex = 0;                             // Saves the starting position for 'y'.
-            int perRow = 2;                                 // Defines the max number of columns.
-            int maxIndex = perRow;                          // Defines the max index to be printed on a single row in the console.
-            int countRows = 0;                              // Holds the number of rows printed to the console.
-
-            if(readPlantInfo.GetLength(1) == 1) {
+            
+            if(readPlantInfo.GetLength(1) < perRow) {
                 maxIndex = readPlantInfo.GetLength(1);      // Reinitializes 'maxIndex' if the length of dimension 1 in the array is '1'.
             }
 
             // Increments through the array, outputting two columns per row in the console.
-            while(readPlantInfo.GetLength(1) > y) {
-                for(int x = 0; x < readPlantInfo.GetLength(0); x++) {
-                    for(; y < maxIndex; y++) {
-                        Console.Write($"{readPlantInfo[x, y], -60}");
+            while (readPlantInfo.GetLength(1) > y) {
+                for (int x = 0; x < readPlantInfo.GetLength(0); x++) {
+                    for (; y < maxIndex; y++) {
+                        //Console.WriteLine($"y: {y}, perRow: {perRow}, maxIndex: {maxIndex}, index: {readPlantInfo[x, y]}, length: {readPlantInfo.GetLength(1)}, countRows: {countRows}, evenRows: {evenRows}");
+                        Console.Write($"{readPlantInfo[x, y],-60}");
                         // Add a new line when the end of the row is reached in the array.
-                        if(maxIndex - 1 == y) {
+                        if (maxIndex - 1 == y) {
                             Console.WriteLine("");
                         }
                     }
@@ -190,7 +208,7 @@ namespace PlantInformationProject {
                 y += perRow;            // Increments 'y' by the number of columns to be printed on one row.
                 startIndex = y;         // Reinitializes the starting position of 'y'.
                 countRows++;
-                maxIndex = (countRows == evenRows) ? maxIndex += 1: maxIndex += perRow;
+                maxIndex = (countRows == evenRows) ? maxIndex += 1 : maxIndex += perRow;
             }
         }
 
@@ -223,19 +241,5 @@ namespace PlantInformationProject {
                 }
             }
         }
-
-        /* Finds the difference between the max number of columns and the number of columns exceeding one row in the output. */
-        /*private static int calcDifference(int size, int difference) {
-            int buffer = 80;        // Amount of space to reserve for each column in the output.
-            
-            // Determines the value of 'difference' .
-            for(int i = 1; i <= size; i++) {
-                if(buffer * i > Console.WindowWidth) {
-                    difference++;
-                }
-            }
-
-            return difference;
-        }*/
     }
 }
